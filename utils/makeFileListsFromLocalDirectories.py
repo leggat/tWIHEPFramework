42dbd6b4aed628a448c1d48d77fe8f991a72998a
leggat <leggat@cern.ch>
2021-11-19 20:54:44 +0800

    A generic way to produce the file lists to run on

--- utils/makeFileListsFromLocalDirectories.py
+++ utils/makeFileListsFromLocalDirectories.py
@@ -8,27 +8,39 @@ import sys,os,math
 
 #datasets lists the names of the datasets to be processed in this script
 datasets = [
-"tW_top",
-"tW_antitop"
+"dy",
+"doubleMu_2018a",
+"doubleMu_2018b",
+"doubleMu_2018c",
+"doubleMu_2018d",
 ]
 
 #datasetID is a map of MC information ID number for each sample name
 datasetID = {
-"tW_top":500026,
-"tW_antitop":500027
+"dy":600000,
+"doubleMu_2018a":400000,
+"doubleMu_2018b":400001,
+"doubleMu_2018c":400002,
+"doubleMu_2018d":400003,
+
 }
 
 #A map of a list of directories containing files to be associated by the sample name
 datasetDirs = {
-"tW_antitop":["/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHext1updatedEleJets/180508_154320/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHext2updatedEleJets/180517_145622/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHupdatedEleJets/180508_154202/0000/"],
-"tW_top":["/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHext1updatedEleJets/180508_153925/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHext2updatedEleJets/180508_154046/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHupdatedEleJets/180508_153756/0000/"]
+"doubleMu_2018a":["/publicfs/cms/data/TopQuark/nanoAOD/data/UL2018A/"],
+"doubleMu_2018b":["/publicfs/cms/data/TopQuark/nanoAOD/data/UL2018B/"],
+"doubleMu_2018c":["/publicfs/cms/data/TopQuark/nanoAOD/data/UL2018C/"],
+"doubleMu_2018d":["/publicfs/cms/data/TopQuark/nanoAOD/data/UL2018D/"],
+"dy":["/publicfs/cms/data/TopQuark/nanoAOD/mc/dy_ll/"]
+#"tW_antitop":["/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHext1updatedEleJets/180508_154320/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHext2updatedEleJets/180517_145622/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_antitop_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_antitop_NFHupdatedEleJets/180508_154202/0000/"],
+#"tW_top":["/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHext1updatedEleJets/180508_153925/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHext2updatedEleJets/180508_154046/0000/","/publicfs/cms/data/TopQuark/cms13TeV/tWlJetSamples/mc/updatedEleJets/ST_tW_top_5f_NoFullyHadronicDecays_13TeV-powheg_TuneCUETP8M1/crab_tW_top_NFHupdatedEleJets/180508_153756/0000/"]
 }
 
 #The number of files to be put into each file list. A smaller number means each condor job will take less time, at the expense of more required jobs
-namesPerFile = 10
+namesPerFile = 1
 
 #the directory to write the lists to
-outputDirectory = "config/files/myFileLists/"
+outputDirectory = "config/files/metFiles/"
 if (not os.path.exists(outputDirectory)):
     os.makedirs(outputDirectory)
 
