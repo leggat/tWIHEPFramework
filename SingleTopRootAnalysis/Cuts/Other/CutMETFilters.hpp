20dadf84fd76bd6ebafab55bd18e19dc007a3616
leggat <leggat@cern.ch>
2021-11-19 21:38:06 +0800

    Adding in a class to cut on the MET filters

---
+++ SingleTopRootAnalysis/Cuts/Other/CutMETFilters.hpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+ * CutMETFilters.hpp                                                           *
+ *                                                                            *
+ * Cuts on Missing Et                                                         *
+ *                                                                            *
+ * Derived from HistoCut which is in turn derived from BaseCut                *
+ *                                                                            *
+ * Public Member Functions of CutMETFilters class                              *
+ *    CutMETFilters()                    -- Parameterized Constructor          *
+ *    ~CutMETFilters()                   -- Destructor                         *
+ *    BookHistogram()                   -- Book histograms                    *
+ *    Apply()                           -- Apply cuts and fill histograms     *
+ *    GetCutName()                      -- Returns "CutMETFilters"             *
+ *                                                                            *
+ * Private Data Members of CutMETFilters                                       *
+ *    myTH1F* _hMissingEtBefore         -- Hist of MissingEt before cuts      *
+ *    myTH1F* _hMissingEtAfter          -- Hist of MissingEt after cuts       *
+ *    Int_t _missingEtMin;              -- Minimum Missing Et                 *
+ *    Int_t _missingEtMin;              -- Minimum Missing Et                 *
+ *                                                                            *
+ * History                                                                    *
+ *      15 Dec 2006 - Created by P. Ryan                                      *
+ *****************************************************************************/
+
+#ifndef CutMETFilters_h
+#define CutMETFilters_h
+
+#include "SingleTopRootAnalysis/Base/Dictionary/HistoCut.hpp"
+#include "SingleTopRootAnalysis/Base/Dictionary/EventContainer.hpp"
+
+#include <vector>
+#include <sstream>
+
+class CutMETFilters : public HistoCut 
+{
+
+public:
+
+  // Parameterized Constructor
+  CutMETFilters(EventContainer *EventContainerObj);
+  
+  // Destructor
+  ~CutMETFilters();
+
+  // Get the name of describing the cut
+  inline std::string GetCutName() { return "CutMETFilters"; };
+
+  // Book Histograms
+  void BookHistogram();
+
+  // Apply cut and fill histogram
+  Bool_t Apply();
+
+
+private:
+
+  // Histograms
+  myTH1F* _hMETFilterBefore;    // Histogram Missing Et
+  myTH1F* _hMETFilterAfter;     // Histogram Missing Et
+
+};
+
+
+#endif
+
+
+
+
+
+
+
---
+++ src/Cuts/Other/CutMETFilters.cpp
@@ -0,0 +1,131 @@
+/******************************************************************************
+ * CutMETFilters.cpp                                                       *
+ *                                                                            *
+ * Cuts on the requirement of a good primary vertex                           *
+ *                                                                            *
+ * Derived from HistoCut which is in turn derived from BaseCut                *
+ *                                                                            *
+ *                                                                            *
+ * Public Member Functions of CutMETFilters class                              *
+ *    CutMETFilters()                     -- Parameterized Constructor         *
+ *    ~CutMETFilters()                    -- Destructor                        *
+ *    BookHistogram()                -- Book histograms                       *
+ *    Apply()                        -- Apply cuts and fill histograms        *
+ *    GetCutName()                   -- Returns "CutMETFilters"                *
+ *                                                                            *
+ * Private Data Members of CutMETFilters class                                 *
+ *    myTH1F* _hMissingEtBefore;    -- Hist of PV before cut        *
+ *    myTH1F* _hMissingEtAfter;     -- Hist of PV after cut         *
+ *                                                                            *
+ * History                                                                    *
+ *      5th July 2016 - Created by Duncan Leggat                              *
+ *****************************************************************************/
+
+#include "SingleTopRootAnalysis/Cuts/Other/CutMETFilters.hpp"
+#include <iostream>
+
+using namespace std;
+
+/******************************************************************************
+ * CutMETFilters::CutMETFilters(EventContainer *EventContainerObj, TString electronType)    *
+ *                                                                            *
+ * Parameterized Constructor                                                  *
+ *                                                                            *
+ * Input:  Event Object class                                                 *
+ * Output: None                                                               *
+ ******************************************************************************/
+CutMETFilters::CutMETFilters(EventContainer *EventContainerObj)
+{
+  // Set Event Container
+  SetEventContainer(EventContainerObj);
+} // CutMETFilters
+
+
+/******************************************************************************
+ * CutMETFilters::~CutMETFilters()                                              *
+ *                                                                            *
+ * Destructor                                                                 *
+ *                                                                            *
+ * Input:  None                                                               *
+ * Output: None                                                               *
+ ******************************************************************************/
+CutMETFilters::~CutMETFilters()
+{
+  
+}//~CutMETFilters
+
+/******************************************************************************
+ * void CutMETFilters::BookHistogram()                                         *
+ *                                                                            *
+ * Book Histograms                                                            *
+ *                                                                            *
+ * Input:  None                                                               *
+ * Output: None                                                               *
+ ******************************************************************************/
+void CutMETFilters::BookHistogram(){
+  
+  // ***********************************************
+  // Book Histograms
+  // ***********************************************  
+
+  // Histogram before cut
+  _hMETFilterBefore =  DeclareTH1F("METFilterBefore","MET Filters Before", 2, 0.0, 2.);
+  _hMETFilterBefore -> SetXAxisTitle("MET Filter");
+  _hMETFilterBefore -> SetYAxisTitle("Events");
+
+  // Histogram after cut
+  _hMETFilterAfter=  DeclareTH1F("METFilterAfter", "MET Filters After", 2, 0.0, 2.);
+  _hMETFilterAfter-> SetXAxisTitle("MET Filter");
+  _hMETFilterAfter-> SetYAxisTitle("Events");
+
+
+  // ***********************************************
+  // Add these cuts to the cut flow table
+  // ***********************************************
+
+  GetCutFlowTable()->AddCutToFlow("MET.Filters","Pass MET filter");
+
+}//BookHistograms()
+
+/******************************************************************************
+ * Bool_t CutMETFilters::Apply()                                                *
+ *                                                                            *
+ * Apply cuts and fill histograms                                             *
+ *                                                                            *
+ * Cut on the selected trigger                                                *
+ *                                                                            *
+ * Input:  None                                                               *
+ * Output: kTrue if successful                                                *
+ ******************************************************************************/
+Bool_t CutMETFilters::Apply()
+{
+
+  EventContainer *EventContainerObj = GetEventContainer();
+
+  Bool_t passesMETFilter = EventContainerObj->passesMETFilters;
+
+  _hMETFilterBefore->Fill(passesMETFilter);
+
+  if (passesMETFilter){
+    GetCutFlowTable()->PassCut("MET.Filters");
+    _hMETFilterAfter->Fill(passesMETFilter);
+  }
+  else {
+    GetCutFlowTable()->FailCut("MET.Filters");
+  }
+  
+  return passesMETFilter;
+
+} //Apply
+
+
+
+
+
+
+
+
+
+
+
+
