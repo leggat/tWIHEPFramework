42b244bc9fbc5ce3594b62c9cd5e338af1d4a9e1
leggat <leggat@cern.ch>
2021-12-16 02:24:45 +0800

    I dunno, tried to add threading and it failed

--- utils/plotFromFiles.py
+++ utils/plotFromFiles.py
@@ -1,10 +1,12 @@
 #Script will plot all of the items from a given selection of samples and files
 
-import sys,os
+import sys,os,threading
 
 from ROOT import *
 
-import genericPlottingMacro
+import subprocess
+
+from genericPlottingClass import GenericPlottingClass
 
 #allow command line argument implementation
 from optparse import OptionParser
@@ -20,8 +22,12 @@ parser.add_option("-q","--quiet",dest="quiet",default=False,action="store_true",
 (options, args) = parser.parse_args()
 
 #Add a string if you want to ignore plots with that name. Useful to include numbers in the framework to skip out whole sections.
-ignorePlots = ["00_","01_","02_","05_","06_","07_","08_","09_","Jet4","Jet5","Jet6","JES","JER","CutEff"] #Ignore the weight variables
+ignorePlots = ["00_","01_","02_","05_","06_","07_","08_","Jet4","Jet5","Jet6","JES","JER","CutEff"] #Ignore the weight variables
+ignorePlots = ["00_","01_","02_","03_","04_""05_","06_","07_","08_","Jet4","Jet5","Jet6","Jet7","JES","JER","CutEff"] #Ignore the weight variables
 #ignorePlots = ["00_","01_","02_","03_",_] #Ignore the weight variables
+
+includePlots = ["METPhi"]
+includePlots = []
 
 customWeightsAllYears={
 "2017":{
@@ -29,29 +35,30 @@ customWeightsAllYears={
   "data":0.818,
   "ww":2.294,
   "zz":1.000,
-  "dy":1.351,
+  "dy":1.296,
   "ttbar_2l":1.000,
   "wz":1.072,
    },
  "electron":{
-  "data":0.817,
+  "data":0.877,
   "ww":2.294,
   "zz":1.000,
-  "dy":1.351,
+  "dy":1.296,
   "ttbar_2l":1.000,
   "wz":1.072,
    },
  },
-"2016":{ "muon":{
+"2016":{
+ "muon":{
   "data":0.919,
   "ww":2.334,
   "zz":1.092,
-  "dy":1.019,
+  "dy":1.007,
   "ttbar_2l":1.000,
   "wz":2.252,
    },
  "electron":{
-  "data":0.694,
+  "data":0.900,
   "ww":2.334,
   "zz":1.092,
   "dy":1.007,
@@ -79,19 +86,19 @@ customWeightsAllYears={
  },
 "2016APV":{
  "muon":{
-  "data":0.745,
-  "ww":1.523,
+  "data":0.752,
+  "ww":1.296,
   "zz":1.024,
-  "dy":1.090,
-  "ttbar_2l":1.923,
+  "dy":1.000,
+  "ttbar_2l":1.000,
   "wz":1.060,
    },
  "electron":{
-  "data":0.877,
-  "ww":1.523,
+  "data":0.875,
+  "ww":1.296,
   "zz":1.024,
-  "dy":1.090,
-  "ttbar_2l":1.923,
+  "dy":1.000,
+  "ttbar_2l":1.000,
   "wz":1.060,
    },
  },
@@ -101,11 +108,28 @@ lumiStrings = {
 "2018":"60",
 "2017":"41",
 "2016":"16",
-"2016APV":"20"
+"2016APV":"20",
+"2016Total":"36"
 }
 
-    
-def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSample,customWeights):
+#Let us run all directories concurrently
+class myPlottingThread(threading.Thread):
+    def __init__(self,inDir,outDir,samples,dataSamples,options,histoGramPerSample,customWeights,genericPlottingMacro):
+        threading.Thread.__init__(self)
+        self.inDir = inDir
+        self.outDir = outDir
+        self.samples = samples
+        self.dataSamples = dataSamples
+        self.options = options
+        self.histoGramPerSample = histoGramPerSample
+        self.customWeights = customWeights
+        self.genericPlottingMacro = genericPlottingMacro
+    def run(self):
+        print "Running {0}".format(inDir)
+        makePlotsInOneDir(self.inDir,self.outDir,self.samples,self.dataSamples,self.options,self.histoGramPerSample,self.customWeights,self.genericPlottingMacro)
+        print "Done {0}".format(inDir) 
+
+def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSample,customWeights,genericPlottingMacro):
 
     #collect together the MC files
     inFiles = {}
@@ -162,6 +186,12 @@ def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSampl
             if ignore in plot:
                 doPlot = False
                 break
+        for includeKeyword in includePlots:
+#            print includeKeyword
+            if includeKeyword in plot:
+                doPlot = True
+                break
+            else: doPlot = False
         if not doPlot: continue
 
         if not options.quiet: print "Doing plot {0}".format(plot)
@@ -184,7 +214,6 @@ def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSampl
 
         if not options.mcOnly:
             for dataSample in dataSamples:
-#                print dataSample
                 if not "data_obs" in histMap.keys():
 #                    if not dataFiles[dataSample].Get(plot): continue
 #                    print dataSample
@@ -194,7 +223,7 @@ def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSampl
                     histMap["data_obs"].Add(dataFiles[dataSample].Get(plot))
 
         #make the plot
-        genericPlottingMacro.makeASingleStackPlot(histMap,saveName,doData=(not options.mcOnly))
+        genericPlottingMacro.makeASingleStackPlot(histMap,saveName,doData=(not options.mcOnly),xAxisLabel=histMap[samples[0]].GetXaxis().GetTitle())
         
         #make a log version
         genericPlottingMacro.makeASingleStackPlot(histMap,saveName+"_log",doData=(not options.mcOnly),isLog=True,xAxisLabel=histMap[samples[0]].GetXaxis().GetTitle())
@@ -207,16 +236,47 @@ def makePlotsInOneDir(inDir,outDir,samples,dataSamples,options,histoGramPerSampl
     latexFile.write("\\end{document}")
 
 
+def dirtyCombine2016():
+
+    for lepton in ["electron","muon"]:
+
+        elePostfix = "" if lepton == "muon" else "Ele"
+
+        pathBase = "met16Total{0}".format(elePostfix)
+
+        if not os.path.exists(pathBase):
+            for dmc in ["","Data"]:
+                os.mkdir("{0}{1}".format(pathBase,dmc))
+            
+        comps1 = AnalysisComponents("met",era="2016",lepton=lepton)
+        comps2 = AnalysisComponents("met",era="2016APV",lepton=lepton)
+        
+        for sample in comps1.samples:
+            if not os.path.exists("{0}/{1}".format(pathBase,sample)):
+                os.makedirs("{0}/{1}/hists/".format(pathBase,sample))
+            subprocess.call("hadd -f {0}/{1}/hists/merged{1}.root met16{2}/{1}/hists/merged{1}.root met16APV{2}/{1}/hists/merged{1}.root".format(pathBase,sample,elePostfix),shell=True)
+        for comp in [comps1,comps2]:
+            for dataSample in comp.dataSamples:
+                if not os.path.exists("{0}Data/{1}".format(pathBase,dataSample)): os.makedirs("{0}Data/{1}/hists/".format(pathBase,dataSample))
+                subprocess.call("cp met{0}{2}Data/{1}/hists/merged{1}.root {3}Data/{1}/hists/merged{1}.root".format(comp.year[2:],dataSample,elePostfix,pathBase),shell=True)
+
 #Main loop
 if __name__ == "__main__":
 
-    years = ["2018","2017","2016","2016APV"]
-#    years = ["2016"]
-    leptons =  ["muon","electron"]
-#    leptons =  ["electron"]
+    dirtyCombine2016()
 
-    genericPlottingMacro.setupEnv()
-    genericPlottingMacro.setRatioWidth(1.)
+#    exit()
+
+    years = ["2018","2017","2016","2016APV","2016Total"]
+    
+#    years = ["2017","2016","2016APV","2016Total"]
+#    years = ["2016Total"]
+#    years = ["2017"]
+    leptons =  ["muon","electron"]
+    #    leptons =  ["electron"]
+#    leptons = ["muon"]
+
+    threads = []
 
     for year in years:
         for lepton in leptons:
@@ -229,6 +289,11 @@ if __name__ == "__main__":
             
             if not os.path.exists(outDir): os.mkdir(outDir)
 
+            genericPlottingMacro = GenericPlottingClass()
+            
+            genericPlottingMacro.setupEnv()
+            genericPlottingMacro.setRatioWidth(1.)
+
             genericPlottingMacro.setOutDir(outDir)
 
             #Set up plots environment
@@ -238,10 +303,25 @@ if __name__ == "__main__":
             if lepton  == "electron": lepChanString = "ee"
             genericPlottingMacro.changeLabels(latex2=lumiString,text2="{0} channel".format(lepChanString))
 
+            customWeights = {}
+            if year in customWeightsAllYears.keys(): customWeights = customWeightsAllYears[year][lepton]
 
-            customWeights = customWeightsAllYears[year][lepton]
+            if not os.path.exists(inDir):
+                print "Skipping {0}".format(inDir)
+                continue
 
             print "processing {0}".format(inDir)
 
-            makePlotsInOneDir(inDir,outDir,comps.sample,comps.dataSamples,options,comps.histoGramPerSample,customWeights)
+
+            makePlotsInOneDir(inDir,outDir,comps.sample,comps.dataSamples,options,comps.histoGramPerSample,customWeights,genericPlottingMacro)
+#            threading.Thread(target=makePlotsInOneDir,args=[inDir,outDir,comps.sample,comps.dataSamples,options,comps.histoGramPerSample,customWeights,genericPlottingMacro]).start()
+
+#            thread = myPlottingThread(inDir,outDir,comps.sample,comps.dataSamples,options,comps.histoGramPerSample,customWeights,genericPlottingMacro)
+#            thread.start()
+
+#            threads.append(thread)
+            
+#    for t in threads:
+#        t.join()
+#    print "Finished all"
     
